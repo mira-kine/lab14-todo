a0d33f9e23cb8af4035e8dc36d8454b29a3cea48
Brett Seifried <seifriedbrett@gmail.com>
2021-10-28 15:25:10 -0700

    added psuedo code to utils.js

---
+++ utils.js
@@ -0,0 +1,31 @@
+export function findById(items, id) {
+    for (let item of items) {
+        if ((item.id) === id) {
+            return item;
+        }
+    }
+}
+
+export function setUser() {
+    // info from login listener
+        // 'submit' button
+    // stringify and push to localStorage
+        //'user-name'
+        //'user-password'
+        // to-do items
+}
+
+
+export function getUser() {
+    // GET -- take from local storage
+    //stringify
+    // push to local storage
+}
+
+
+export function renderList()
+
+// export function addItem() - will use eventlistener instead.
+
+export function removeItem()
+
